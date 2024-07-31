a0cba121faf0407664ba1ae1730c7574d9a1ea17
Ankit kumar <150576490+Kumarankit666@users.noreply.github.com>
2024-08-01 01:34:26 +0530

    Convert Foot into CM

---
+++ Convert.py
@@ -0,0 +1,8 @@
+name = input('Please enter your name:- ')
+print('Hello ' + name )
+
+# enter only integer value
+
+height = int(input(name + ' please enter your height in Foot:- '))
+ft_cm = (height*30.48)
+print(f'{name} your height in CM is:- {ft_cm}')
